f768a5e6a8330ef0baf65813948c5ec4a008479f
Cedric Joaquin <joaquin.cedric@gmail.com>
2020-05-08 16:58:41 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,8 +8,10 @@
 
 def select_winner(passengers)
   passengers.each do |room, name|
-    if room.include?("a") && name.downcase.include?("a")
-      lucky_winner = name 
+    if room == :suite_a && name.start_with?("A")
+      winner = name 
     end
   end
 end
+
+winner
